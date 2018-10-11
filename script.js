f08c3be99915b3d0ae149f45c85fdd85fd2434de
Your Name <you@example.com>
2018-10-11 23:16:18 +0200

    testing stage

---
+++ script.js
@@ -0,0 +1,20 @@
+
+var index = 0;
+
+function moveZombie() {
+//  const button = document.getElementById("click");
+    const zombie = document.getElementById("image");
+
+//   zombie.style.right = "4em";
+
+    console.log('hello');
+};
+
+function test(){
+for(i = 0; i < 5; i++){
+    
+    //moveZombie()
+setInterval(moveZombie, 1000);
+}
+}
+
